3bfa86952d29d63736cdc9575f0ff1e3088eb69f
shankutanna <147415639+shankutanna@users.noreply.github.com>
2026-01-14 21:10:55 +0530

    commented out findbyusername

--- src/main/java/com/enterprisesystemengineering/Entity/UserRepository.java
+++ src/main/java/com/enterprisesystemengineering/Entity/UserRepository.java
@@ -9,6 +9,6 @@ public interface UserRepository extends JpaRepository<User, String> {
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
 
-     Optional<User> findByUsername(String username);
+    # Optional<User> findByUsername(String username);
 }
 
